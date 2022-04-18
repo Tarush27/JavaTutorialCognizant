c60570535a76d0ed9e595a627cfecb138a19341b
Tarushkaistha27 <tarushkaistha05@gmail.com>
2022-04-18 12:37:40 +0530

    unhandled exception

--- src/generics/Node.java
+++ src/generics/Node.java
@@ -29,3 +29,23 @@ class Node<T> { */
         Node<Shape>  ns=nc;
         Answer:No.Because Node<Circle> is not a subtype of Node<Shape>.
 */
+package generics;
+
+class Node<T> {
+    public static void main(String[] args) {
+//        Node<Circle> nc = new Node<Circle>();
+//        Node<Shape> ns = nc;
+    }
+}
+
+class Shape {
+
+}
+
+class Circle extends Shape {
+
+}
+
+class Rectangle extends Shape {
+
+}
