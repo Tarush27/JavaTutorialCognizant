74da1efe4b542ca99aa5bbbb13cced3188d40c42
Tarushkaistha27 <tarushkaistha05@gmail.com>
2022-04-18 11:46:12 +0530

    check max of two numbers using generics.

--- src/generics/q2.java
+++ src/generics/q2.java
@@ -1,3 +1,5 @@
+package generics;
+
 /*
 
 Will the following class compile? If not, why?
@@ -10,3 +12,18 @@ public final class Algorithm {
 Answer: No. The greater than (>) operator applies only to primitive numeric types.
 
 */
+class q2{
+    /*public static <T> T max(T x, T y){
+        return x > y ? x : y;
+    }*/
+
+    public static <T extends Comparable<T>> T max(T x,T y){
+        if(x.compareTo(y) > 0){
+            return x;
+        }
+        return y;
+    }
+    public static void main(String[] args) {
+        System.out.println(q2.max(3,2));
+    }
+}
