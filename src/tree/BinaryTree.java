b9b4a33a73c74c65d1b830ccfcca01e361f20803
Tarushkaistha27 <tarushkaistha05@gmail.com>
2022-05-25 07:48:12 +0530

    implementation of tree.

---
+++ src/tree/BinaryTree.java
@@ -0,0 +1,106 @@
+package tree;
+
+class Node {
+    int value;
+    Node left;
+    Node right;
+
+    Node(int value) {
+        this.value = value;
+        right = null;
+        left = null;
+    }
+}
+
+public class BinaryTree {
+    static Node root;
+
+    // Constructors
+    BinaryTree(int key) {
+        root = new Node(key);
+    }
+
+    BinaryTree() {
+        root = null;
+    }
+
+    public void insert(Node node, int value) {
+        if (value < node.value) {
+            if (node.left != null) {
+                insert(node.left, value);
+            } else {
+                System.out.println(" Inserted " + value + " to left of " + node.value);
+                node.left = new Node(value);
+            }
+        } else if (value > node.value) {
+            if (node.right != null) {
+                insert(node.right, value);
+            } else {
+                System.out.println("  Inserted " + value + " to right of "
+                        + node.value);
+                node.right = new Node(value);
+            }
+        }
+    }
+
+    public void traverseInOrder(Node node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            System.out.print(" " + node.value);
+            traverseInOrder(node.right);
+        }
+    }
+
+    public void traversePreOrder(Node node) {
+        if (node != null) {
+            System.out.print(" " + node.value);
+            traversePreOrder(node.left);
+            traversePreOrder(node.right);
+        }
+    }
+
+    public void traversePostOrder(Node node) {
+        if (node != null) {
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            System.out.print(" " + node.value);
+        }
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        /*create root*/
+        root = new Node(1);
+
+        /* following is the tree after above statement
+
+              1
+            /   \
+          null  null     */
+
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        /* 2 and 3 become left and right children of 1
+               1
+            /     \
+          2        3
+        /   \     /  \
+      null null null null  */
+
+        root.left.left = new Node(4);
+
+        tree.insert(root, 2);
+        tree.insert(root, 4);
+        tree.insert(root, 8);
+        tree.insert(root, 6);
+        tree.insert(root, 7);
+        tree.insert(root, 3);
+        tree.insert(root, 9);
+        System.out.println("Traversing tree in order");
+        tree.traverseInOrder(root);
+        tree.traversePreOrder(root);
+        tree.traversePostOrder(root);
+    }
+}
